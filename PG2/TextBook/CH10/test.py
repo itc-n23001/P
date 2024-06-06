7286f1f99098baf5ad3bcc51f6509e22e0fc51ca
itc-n23001 <n23001@std.it-college.ac.jp>
2024-06-06 00:48:53 +0000

    試作用プログラミング

--- PG2/TextBook/CH10/test.py
+++ PG2/TextBook/CH10/test.py
@@ -1,5 +1,7 @@
 import os #osをインポート
+import shutil
+
 for a,b,c in os.walk("test"):
     for f in c: 
       if f.endswith((".jpg",".pdf")): 
-            print(os.path.join(a,f))
+            print(os.path.join(a,f)) 

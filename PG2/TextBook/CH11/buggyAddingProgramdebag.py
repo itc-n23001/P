d3987afa4b7daee05474ec3a45fe674c328fa37e
itc-n23001 <n23001@std.it-college.ac.jp>
2024-06-11 03:24:10 +0000

    debagprograming

---
+++ PG2/TextBook/CH11/buggyAddingProgramdebag.py
@@ -0,0 +1,13 @@
+rint('足し合わせる第１の数を入力して下さい:')
+first = input()
+print('足し合わせる第２の数を入力して下さい:')
+second = input()
+print('足し合わせる第３の数を入力して下さい:')
+third = input()
+
+first_number = float(first)
+second_number = float(second)
+third_number =  float(third)
+
+total = first_number + second_number + third_number
+print('合計は' + str(total))
